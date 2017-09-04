ebf94cb58b23a057dcbc3448e668d7d2faefac14
InfinitiQ70 <29373550+InfinitiQ70@users.noreply.github.com>
2017-09-04 12:30:44 +0800

    创建A.java文件
    第一次使用github创建项目文件。

---
+++ A.java
@@ -0,0 +1,17 @@
+package com.oaec.test;
+
+public class A {
+	
+	public A() {
+		System.out.println("A的无参构造");
+	}
+	
+	{
+		System.out.println("A的方法");
+	}
+	
+	static{
+		System.out.println("A的静态代码块");
+	}
+
+}
