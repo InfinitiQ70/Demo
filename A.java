7df86adb0003a7fe8aeb35fe9fe826ccf993e344
InfinitiQ70 <29373550+InfinitiQ70@users.noreply.github.com>
2017-09-04 12:40:00 +0800

    Update A.java
    第一次使用GitHub更新

--- A.java
+++ A.java
@@ -2,6 +2,7 @@ package com.oaec.test;
 
 public class A {
 	
+	
 	public A() {
 		System.out.println("A的无参构造");
 	}
@@ -11,6 +12,7 @@ public class A {
 	}
 	
 	static{
+		
 		System.out.println("A的静态代码块");
 	}
 
